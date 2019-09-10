af4049100b9a238494e86365c558aba01c41c81b
B0ER <qwertygo85@gmail.com>
2019-09-10 15:38:15 +0300

    create logger

---
+++ Store.BussinesLogic/Common/ApplicationLoggerProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Store.BussinesLogic.Common
+{
+    public class ApplicationLoggerProvider : ILoggerProvider
+    {
+        private string _filePath;
+
+        public ApplicationLoggerProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new ApplicationLogger(_filePath);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
