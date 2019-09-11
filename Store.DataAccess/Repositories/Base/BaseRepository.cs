bacf31019e114452acc78dce6f865f5f7798d748
B0ER <qwertygo85@gmail.com>
2019-09-11 10:43:57 +0300

    create generic repository with crud

---
+++ Store.DataAccess/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Store.DataAccess.AppContext;
+using Store.DataAccess.Entities.Base;
+using Store.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Store.DataAccess.Repositories.Base
+{
+    public abstract class BaseRepository<TItem> : IGenericRepository<TItem> where TItem : BaseEntity
+    {
+        private ApplicationDbContext _db;
+        public BaseRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(TItem item)
+        {
+            _db.Add<TItem>(item);
+        }
+
+        public void Delete(TItem item)
+        {
+            _db.Remove<TItem>(item);
+        }
+
+        public void DeleteById(long id)
+        {
+            var deletedItem = _db.Set<TItem>().Find(id);
+            _db.Remove(deletedItem);
+        }
+
+        public TItem FindById(long id)
+        {
+            return _db.Set<TItem>().Find(id);
+        }
+
+        public IEnumerable<TItem> GetAll()
+        {
+            return _db.Set<TItem>().ToList();
+        }
+
+        public void Update(TItem item)
+        {
+            _db.Update<TItem>(item);
+        }
+    }
+}
---
+++ Store.DataAccess/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Store.DataAccess.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.DataAccess.Repositories.Interfaces
+{
+    internal interface IGenericRepository<TItem> where TItem : BaseEntity
+    {
+        IEnumerable<TItem> GetAll();
+        TItem FindById(long id);
+        void Add(TItem item);
+        void Delete(TItem item);
+        void DeleteById(long id);
+        void Update(TItem item);
+    }
+}
