26d1810b4f31c8e6278c219d421f2f4a3c28ac05
B0ER <qwertygo85@gmail.com>
2019-09-17 09:57:07 +0300

    add currency

--- Store.DataAccess/Entities/Enums/Currency.cs
+++ Store.DataAccess/Entities/Enums/Currency.cs
@@ -8,6 +8,13 @@ namespace Store.DataAccess.Entities.Enums
     {
         public enum Currency
         {
+            USD = 0,
+            AUD = 1,
+            BYN = 2,
+            EUR = 3,
+            GBP = 4,
+            PLN = 5,
+            UAH = 6
         }
     }
 }
